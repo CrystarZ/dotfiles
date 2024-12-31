0e2d1aeca90c9359e9b036f5388ce6d947d36789
AEva <crystar.z@outlook.com>
2024-12-31 21:16:34 +0800

    add : zsh,git,shtools

---
+++ zsh/.config/zsh/aliases.zsh
@@ -0,0 +1,68 @@
+#!/bin/sh
+#alias j='z'
+#alias f='zi'
+alias g='lazygit'
+alias zsh-update-plugins="find "$ZDOTDIR/plugins" -type d -exec test -e '{}/.git' ';' -print0 | xargs -I {} -0 git -C {} pull -q"
+alias nvimrc='${EDITOR} ~/.config/nvim/'
+alias zshrc='${EDITOR} ~/.zshrc'
+#alias sshk="kitty +kitten ssh"
+# alias lvim="env TERM=wezterm lvim"
+# alias nvim="env TERM=wezterm nvim"
+
+#modern tools
+# alias cd='z'
+alias ls='eza --icons'
+alias grep='rg --fixed-strings --color=auto'
+alias cat='bat -pp'
+alias du='ncdu' #yazi
+alias tree='eza --tree --icons'
+alias find='fd --hidden --follow --color=auto'
+#alias cut='choose'
+alias man='tldr --pager=less'
+alias ps='procs --color=auto'
+alias curl='curlie --color=auto'
+alias vim='nvim'
+
+if command -v bat &> /dev/null; then
+  alias cat="bat -pp --theme \"Visual Studio Dark+\"" 
+  alias catt="bat --theme \"Visual Studio Dark+\"" 
+fi
+
+# confirm before overwriting something
+alias cp="cp -i"
+alias mv='mv -i'
+alias rm='rm -i'
+
+# easier to read disk
+alias df='df -h'     # human-readable sizes
+alias free='free -m' # show sizes in MB
+
+# get top process eating memory
+alias psmem='ps auxf | sort -nr -k 4 | head -5'
+# get top process eating cpu ##
+alias pscpu='ps auxf | sort -nr -k 3 | head -5'
+
+# systemd
+alias systemctl_list="systemctl list-unit-files --state=enabled"
+
+#alias mach_java_mode="export SDKMAN_DIR="$HOME/.sdkman" && [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh""
+
+
+case "$(uname -s)" in
+
+Darwin)
+	# echo 'Mac OS X'
+	alias ls='ls -G'
+	;;
+
+Linux)
+	alias ls='ls --color=auto'
+	;;
+
+CYGWIN* | MINGW32* | MSYS* | MINGW*)
+	# echo 'MS Windows'
+	;;
+*)
+	# echo 'Other OS'
+	;;
+esac
---
+++ zsh/.zshrc
@@ -0,0 +1,40 @@
+#j/bin/sh
+unset ZAP_THEME
+unset PROMPT
+[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# history
+HISTFILE=~/.zsh_history
+
+# source
+plug "$HOME/.config/zsh/aliases.zsh"
+plug "$HOME/.config/zsh/exports.zsh"
+plug "$HOME/.config/zsh/functions.zsh"
+
+# plugins
+plug "esc/conda-zsh-completion"
+plug "zsh-users/zsh-autosuggestions"
+plug "hlissner/zsh-autopair"
+plug "zap-zsh/supercharge"
+plug "zap-zsh/vim"
+# plug "zap-zsh/zap-prompt"
+# plug "zap-zsh/atmachine" 
+plug "zap-zsh/fzf"
+plug "zap-zsh/exa"
+plug "zsh-users/zsh-syntax-highlighting"
+plug "zsh-users/zsh-history-substring-search"
+
+# keybinds
+bindkey '^ ' autosuggest-accept
+
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
+alias :q="exit"
+
+#ENVARS
+
+eval "$(oh-my-posh init zsh --config ${_themes_}packages/oh-my-posh/oneiroi.omp.json)"
