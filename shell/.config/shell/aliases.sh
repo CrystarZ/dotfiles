f07c2c7701fbbdc914e591733f56fd6c23b2a6ff
AEva <crystar.z@proton.me>
2025-01-28 01:14:28 +0800

    refactor(shell): ♻️ basic more human

--- shell/.config/shell/aliases.sh
+++ shell/.config/shell/aliases.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
-#alias j='z'
-#alias f='zi'
 alias g='lazygit'
-alias zsh-update-plugins="find "$ZDOTDIR/plugins" -type d -exec test -e '{}/.git' ';' -print0 | xargs -I {} -0 git -C {} pull -q"
 alias dfrc='${EDITOR} $_dfs_'
 alias thmrc='${EDITOR} $_themes_'
 alias nvimrc='${EDITOR} ~/.config/nvim/'
 alias shrc='${EDITOR} ~/.config/shell/'
 alias zshrc='${EDITOR} ~/.zshrc'
-#alias sshk="kitty +kitten ssh"
-# alias lvim="env TERM=wezterm lvim"
-# alias nvim="env TERM=wezterm nvim"
 
 #modern tools
 # alias cd='z'
@@ -26,11 +20,6 @@ alias mian='tldr' #mini-man
 alias ps='procs --color=auto'
 alias curl='curlie --color=auto'
 alias vim='nvim'
-
-if command -v bat &> /dev/null; then
-  alias cat="bat -pp --theme \"Visual Studio Dark+\"" 
-  alias catt="bat --theme \"Visual Studio Dark+\"" 
-fi
 
 # confirm before overwriting something
 alias cp="cp -i"
@@ -49,22 +38,11 @@ alias pscpu='ps auxf | sort -nr -k 3 | head -5'
 # systemd
 alias systemctl_list="systemctl list-unit-files --state=enabled"
 
-#alias mach_java_mode="export SDKMAN_DIR="$HOME/.sdkman" && [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh""
-
-
 case "$(uname -s)" in
 
-Darwin)
-	# echo 'Mac OS X'
-	;;
+Darwin) ;;
+Linux) ;;
+CYGWIN* | MINGW32* | MSYS* | MINGW*) ;;
+*) ;;
 
-Linux)
-	;;
-
-CYGWIN* | MINGW32* | MSYS* | MINGW*)
-	# echo 'MS Windows'
-	;;
-*)
-	# echo 'Other OS'
-	;;
 esac
--- shell/.config/shell/exports.sh
+++ shell/.config/shell/exports.sh
@@ -1,53 +1,31 @@
 #!/bin/sh
-# HISTFILE="$XDG_DATA_HOME"/zsh/history
-HISTSIZE=1000000
-SAVEHIST=1000000
 export EDITOR="nvim"
-export TERMINAL="Windows Terminal"
-# export BROWSER="firefox"
-export PATH="/opt/homebrew/bin:$PATH"
-export PATH="/home/linuxbrew/.linuxbrew/opt/man-db/libexec/bin:$PATH" #man
-export PATH="$HOME/.local/bin":$PATH
-export PATH="$HOME/bin/shtools":$PATH
-#export PATH="$HOME/.docker/bin":$PATH
-#export PATH="$HOME/.local/nvim-macos-arm64/bin":$PATH
+export TERMINAL="wezterm"
+export BROWSER="firefox"
 export MANPAGER='nvim +Man!'
 export MANWIDTH=999
-#export PATH=$HOME/.cargo/bin:$PATH
-#export PATH=$HOME/.local/share/go/bin:$PATH
-#export GOPATH=$HOME/.local/share/go
-#export PATH=$HOME/.fnm:$PATH
-#export PATH="$HOME/.local/share/neovim/bin":$PATH
-#export PATH="$HOME/.local/share/bob/nvim-bin":$PATH
-#export XDG_CURRENT_DESKTOP="Wayland"
+# export XDG_CURRENT_DESKTOP="Wayland"
+
 export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_ENV_HINTS=1
-#export PATH="$PATH:./node_modules/.bin"
-#eval "$(fnm env)"
 
-export _dfs_="$HOME/DotFiles/"
-export _rws_="$HOME/_EVOLINK_/"
+#PATH
+export PATH="$HOME/.local/bin":$PATH
+export PATH="$HOME/.local/share/neovim/bin":$PATH
+export PATH="$HOME/.node_modules/.bin":$PATH
+
+export _rws_="$HOME/"
 export _ws_="$HOME/WorkSpace/"
 export _configs_="$HOME/.config/"
-export _themes_="${_rws_}Themes/oneiroi-theme/themes"
-
-#proxy
-host_ip=$(cat /etc/resolv.conf | grep "nameserver" | cut -f 2 -d " ")
-export http_proxy="http://$host_ip:7890"
-export https_proxy="http://$host_ip:7890"
+export _dfs_="${_rws_}DotFiles/"
+export _dfsl_="${_rws_}DotFiles.local/"
+export _themes_="${_rws_}Themes/oneiroi-theme/themes/"
 
 case "$(uname -s)" in
 
-Darwin)
-  # echo 'Mac OS X'
-  ;;
-
+Darwin) ;;
 Linux) ;;
+CYGWIN* | MINGW32* | MSYS* | MINGW*) ;;
+*) ;;
 
-CYGWIN* | MINGW32* | MSYS* | MINGW*)
-  # echo 'MS Windows'
-  ;;
-*)
-  # echo 'Other OS'
-  ;;
 esac
