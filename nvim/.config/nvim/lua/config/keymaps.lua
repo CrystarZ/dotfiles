8fdfbad9a918da6221d766046fa55293f725773b
AEva <crystar.z@outlook.com>
2025-02-02 21:26:36 +0800

    style(nvim): 🎨 format keymaps.lua

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -7,18 +7,17 @@ local function map(mode, l, r, desc)
   vim.keymap.set(mode, l, r, { noremap = true, silent = true, desc = desc })
 end
 
---fuction
-map("n", "<F2>", ":set nu! nu?<CR>", "togle line number") --line number
+--line number
+map("n", "<F2>", ":set nu! nu?<CR>", "togle line number")
+
 --JK
-map("i", "jk", "<ESC>", "JK")
-map("v", "jk", "<ESC>", "JK")
+map({ "i", "v" }, "jk", "<ESC>", "JK")
 map("t", "<ESC>", "<C-\\><C-n>", "esc teminal")
 map("t", "<C-/>", "<C-\\><C-n>:bdelete! %<CR>", "close terminal")
 map("t", "<C-_>", "<C-\\><C-n>:bdelete! %<CR>", "close terminal")
 
---ctrl-a
-map("n", "<leader>a", "ggVG", "select all") --Ctrl-A
-map("i", "<leader>a", "<ESC>ggVG", "select all")
-map("v", "<leader>a", "<ESC>ggVG", "select all")
+--Ctrl-A
+map({ "n", "i", "v" }, "<leader>a", "<ESC>ggVG", "select all")
 
-map("n", "U", "<C-r>", "redo") -- redo
+-- redo
+map("n", "U", "<C-r>", "redo")
