f64313ce58f4995d900ab9a6abd80ae0c16ae9aa
AEva <crystar.z@outlook.com>
2025-02-12 06:08:30 +0800

    feat(init): ✨ add tmux reload,add path config

--- shell/.config/shell/themes/init.sh
+++ shell/.config/shell/themes/init.sh
@@ -2,10 +2,11 @@
 
 theme() {
   local t=$1
-  sed -i -E --follow-symlinks "s|(export SHENV_THEME=).*|\1${t}|" "${HOME}/.zshrc"
-  sed -i -E --follow-symlinks "s|(export SHENV_THEME=).*|\1${t}|" "${HOME}/.bashrc"
+  local p=${SHENV_SWITCH_PATH:-$HOME/.config/shell/themes}
+  eval "$p/main.sh" "$t" #main
   export SHENV_THEME=${t}
-  source ~/.config/shell/themes.sh
-  ~/.config/shell/themes/main.sh $t
+  sed -i -E --follow-symlinks "s|(export SHENV_THEME=).*|\1${t}|" "${HOME}/.zshrc"
+  sed -i -E --follow-symlinks "s|(export SHENV_THEME=).*|\2${t}|" "${HOME}/.bashrc"
+  source ~/.config/shell/themes.sh #reload prompt
+  tmux source ~/.tmux.conf         #reload tmux
 }
-
