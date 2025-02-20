4c5be065c1aab756b602dc5ecbd605e01f03a069
AEva <crystar.z@proton.me>
2025-02-20 09:05:01 +0800

    feat(nvim): ✨ add some extra plugins,remove useless configs

--- nvim/.config/nvim/lua/plugins/extra.lua
+++ nvim/.config/nvim/lua/plugins/extra.lua
@@ -1,7 +1,15 @@
 return {
   -- 启用 extras 模块
+
+  { import = "lazyvim.plugins.extras.coding.blink" },
+  { import = "lazyvim.plugins.extras.editor.fzf" },
+  { import = "lazyvim.plugins.extras.editor.neo-tree" },
+  { import = "lazyvim.plugins.extras.editor.inc-rename" },
+  { import = "lazyvim.plugins.extras.editor.snacks_picker" },
   { import = "lazyvim.plugins.extras.dap.core" },
   { import = "lazyvim.plugins.extras.ui.treesitter-context" },
   { import = "lazyvim.plugins.extras.ui.smear-cursor" },
+  { import = "lazyvim.plugins.extras.util.dot" },
+  { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
   -- { import = "lazyvim.plugins.extras.ui.edgy" },
 }
