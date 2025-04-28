6b27cb2c9ca131ff560b920fbf99495561f35bb9
AEva <crystar.z@outlook.com>
2025-04-29 06:52:35 +0800

    fix(shell): :bug: fix some warnings

--- shell/.config/shell/init.sh
+++ shell/.config/shell/init.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-export SHENV_SHELL=${SHENV_SHELL:=$(basename $(readlink /proc/$$/exe))} #shell = bash|zsh
-export SHENV_THEME=${SHENV_THEME:="oneiroi-dream"}                      #theme = oneiroi-dream|oneiroi-melatonin
-export SHENV_PROMPT=${SHENV_PROMPT:="starship"}                         #prompt = none|omp|starship"
+export SHENV_SHELL="${SHENV_SHELL:=$(basename "$(readlink /proc/$$/exe)")}" #shell = bash|zsh
+export SHENV_THEME="${SHENV_THEME:="oneiroi-dream"}"                        #theme = oneiroi-dream|oneiroi-melatonin
+export SHENV_PROMPT="${SHENV_PROMPT:="starship"}"                           #prompt = none|omp|starship"
 
 SHENV_HOME=${SHENV_HOME:="$HOME/.config/shell/"}
 SHENV_ALIASES=${SHENV_ALIASES:=true}
@@ -15,22 +15,22 @@ SHENV_INIT_CONDA=${SHENV_INIT_CONDA:=false}
 export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_ENV_HINTS=1
 export HOMEBREW_PREFIX="${HOMEBREW_PREFIX:-/home/linuxbrew/.linuxbrew}"
-[ -f "${HOMEBREW_PREFIX}/bin/brew" ] && eval "$(${HOMEBREW_PREFIX}/bin/brew shellenv)"
+[ -f "${HOMEBREW_PREFIX}/bin/brew" ] && eval "$("${HOMEBREW_PREFIX}/bin/brew" shellenv)"
 
 cmdv() {
   command -v "$@" >/dev/null 2>&1
 }
 
 #source
-[[ $SHENV_ALIASES == "true" ]] && source "$SHENV_HOME/aliases.sh"
-[[ $SHENV_EXPORTS == "true" ]] && source "$SHENV_HOME/exports.sh"
-[[ $SHENV_FUNCTIONS == "true" ]] && source "$SHENV_HOME/functions.sh"
-[[ $SHENV_THEMES == "true" ]] && source "$SHENV_HOME/themes.sh"
+[ "$SHENV_ALIASES" = "true" ] && . "$SHENV_HOME/aliases.sh"
+[ "$SHENV_EXPORTS" = "true" ] && . "$SHENV_HOME/exports.sh"
+[ "$SHENV_FUNCTIONS" = "true" ] && . "$SHENV_HOME/functions.sh"
+[ "$SHENV_THEMES" = "true" ] && . "$SHENV_HOME/themes.sh"
 
 # >>> ENV INITIALIZE >>>
 
-cmdv zoxide && eval "$(zoxide init $SHENV_SHELL)"
-cmdv fzf && eval "$(fzf --$SHENV_SHELL)"
+cmdv zoxide && eval "$(zoxide init "$SHENV_SHELL")"
+cmdv fzf && eval "$(fzf --"$SHENV_SHELL")"
 
 # >>> conda initialize >>>
 export CONDA_HOME="${CONDA_HOME:-$HOME/miniconda3}"
@@ -46,5 +46,5 @@ ci() {
   echo "$(tput setab 4)$(tput setaf 0)CONDA$(tput sgr0) use conda activate {env}"
 }
 
-[[ $SHENV_INIT_CONDA == "true" ]] && ci
+[ "$SHENV_INIT_CONDA" = "true" ] && ci
 # <<< conda initialize <<<
