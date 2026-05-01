359def8c79aa47d25b359268baaacc6783421e35
AEva <crystar.z@outlook.com>
2026-05-01 21:33:35 +0000

    fix(shell.pkg.prompt): starship default use powerline style

--- shell/.config/shell/pkgs/prompt.sh
+++ shell/.config/shell/pkgs/prompt.sh
@@ -10,6 +10,6 @@ export POSH_DISABLE_UPDATE=true
 OMP_CONFIG="${SHENV_THEME_HOME}/oh-my-posh/${THEME}.omp.json"
 [ "$PROMPTYPE" = "omp" ] && command -v oh-my-posh >/dev/null && eval "$(oh-my-posh init "$SHENV_SHELL" --config "$OMP_CONFIG")"
 
-export STARSHIP_CONFIG="${SHENV_THEME_HOME}/starship/${THEME}.toml"
+export STARSHIP_CONFIG="${SHENV_THEME_HOME}/starship/${THEME}-powerline.toml"
 [ "$PROMPTYPE" = "starship" ] && command -v starship >/dev/null && eval "$(starship init "$SHENV_SHELL")"
 # <<< prompt initialize <<<
